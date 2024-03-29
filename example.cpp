56ceedae62c108d9d01803eafcdab2f2eb717477
QY Li <m13386474590@163.com>
2024-03-29 11:32:15 +0800

    优化示例，以实现有返回值和无返回值的不同示例

--- example.cpp
+++ example.cpp
@@ -8,18 +8,32 @@ using namespace std;
 int main()
 {
 	ThreadPool::Pool pool(16);
-	vector<shared_ptr<ThreadPool::Task<int>>> results;
 
+	vector<shared_ptr<ThreadPool::Task<void>>> voidResults;
 	for (int i = 0; i < 800; ++i)
 	{
-		results.push_back(pool.AddTask([i]() {cout << "Count: " << i << endl; return i; }));
+		voidResults.push_back(std::move(pool.AddTask([i]() {cout << "Count: " << i << endl; })));
 	}
 	//this_thread::sleep_for(chrono::seconds(5));
-	for (auto& r : results)
+	for (auto& r : voidResults)
+	{
+		cout << r->GetTaskStatus() << endl;
+		//r->GetResult();
+	}
+	std::cout << std::endl;
+	
+	vector<shared_ptr<ThreadPool::Task<int>>> intResults;
+	for (int i = 0; i < 800; ++i)
+	{
+		intResults.push_back(std::move(pool.AddTask([i]() {cout << "Count: " << i << endl; return i; })));
+	}
+	//this_thread::sleep_for(chrono::seconds(5));
+	for (auto& r : intResults)
 	{
 		cout << r->GetTaskStatus() << endl;
 		cout << r->GetResult() << endl;
 	}
 	std::cout << std::endl;
+
 	return 0;
 }
