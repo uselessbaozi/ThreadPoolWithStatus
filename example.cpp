d92b240a631123f59c12f53ccc0505d1f91e259a
QY Li <m13386474590@163.com>
2024-04-03 16:29:41 +0800

    规范示例

--- example.cpp
+++ example.cpp
@@ -5,54 +5,83 @@
 #include <memory>
 using namespace std;
 
-class test
-{
-public:
-	test(int i) : m_i(i) {}
-	void ttt() const
-	{
-		cout << "Count: " << m_i << endl;
-		//return m_i;
-	}
-private:
-	int m_i;
-};
 
-int main()
+void ExampleWithNoReturn()
 {
-	ThreadPool::Pool pool(16);
-
 	vector<shared_ptr<ThreadPool::Task<void>>> voidResults;
 	for (int i = 0; i < 800; ++i)
 	{
-		voidResults.push_back(std::move(pool.AddTask([i]() {cout << "Count: " << i << endl; })));
+		voidResults.push_back(std::move(ThreadPool::Pool::GetPool()->AddTask([i]() {cout << "Count: " << i << endl; })));
 	}
-	pool.WaitAll();
+	ThreadPool::Pool::GetPool()->WaitAll();
 	//this_thread::sleep_for(chrono::seconds(5));
 	for (auto& r : voidResults)
 	{
 		cout << r->GetTaskStatus() << endl;
 		//r->GetResult();
 	}
-	std::cout << std::endl;
-	
+	cout << endl;
+}
+
+void ExampleWithReturn()
+{
 	vector<shared_ptr<ThreadPool::Task<int>>> intResults;
 	for (int i = 0; i < 800; ++i)
 	{
-		intResults.push_back(std::move(pool.AddTask([i]() {cout << "Count: " << i << endl; return i; })));
+		intResults.push_back(std::move(ThreadPool::Pool::GetPool()->AddTask([i]() {cout << "Count: " << i << endl; return i; })));
 	}
-	pool.WaitAll();
+	ThreadPool::Pool::GetPool()->WaitAll();
 	//this_thread::sleep_for(chrono::seconds(5));
 	for (auto& r : intResults)
 	{
 		cout << r->GetTaskStatus() << endl;
 		cout << r->GetResult() << endl;
 	}
-	std::cout << std::endl;
+	cout << endl;
+}
 
-	auto t = test(10);
-	auto result = pool.AddTask(&test::ttt, &t);
-	pool.WaitAll();
+void ExampleWithClass()
+{
+	class test
+	{
+	public:
+		test(int i) : m_i(i) {}
+		void ttt() const
+		{
+			cout << "Count: " << m_i << endl;
+			//return m_i;
+		}
+	private:
+		int m_i;
+	};
+
+	vector<shared_ptr<ThreadPool::Task<void>>> voidResults;
+	vector<test> tests;
+	for (int i = 0; i < 800; ++i)
+	{
+		tests.push_back(test(i));
+	}
+	for (int i = 0; i < 800; ++i)
+	{
+		voidResults.push_back(std::move(ThreadPool::Pool::GetPool()->AddTask(&test::ttt, &tests[i])));
+	}
+	ThreadPool::Pool::GetPool()->WaitAll();
+	//this_thread::sleep_for(chrono::seconds(5));
+	for (auto& r : voidResults)
+	{
+		cout << r->GetTaskStatus() << endl;
+		//r->GetResult();
+	}
+	cout << endl;
+}
+
+int main()
+{
+	ThreadPool::Pool pool(16);
+
+	ExampleWithNoReturn();
+	ExampleWithReturn();
+	ExampleWithClass();
 
 	return 0;
 }
